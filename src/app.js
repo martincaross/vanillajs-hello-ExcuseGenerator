c976b643bd0b3fb4c8ae66bc41dc8ce97872cf28
Martín <martinjcaro@gmail.com>
2025-03-03 12:44:46 +0000

    second commit

--- src/app.js
+++ src/app.js
@@ -1,27 +1,36 @@
 import "bootstrap";
 import "./style.css";
 
+window.onload = function () {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = ["before the class", "when I was sleeping", "during my lunch", "while I was praying"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-  let action = ['ate', 'peed', 'crushed', 'broke'];
-  let what = ['my homework', 'my phone', 'the car'];
-  let when = ['before the class', 'when I was sleeping', 'when I was sleeping', 'during my lunch', 'while I was praying'];
-
-  function ExcuseGenerator(){
+  function generateExcuse() {
     let randomWho = who[Math.floor(Math.random() * who.length)];
     let randomAction = action[Math.floor(Math.random() * action.length)];
     let randomWhat = what[Math.floor(Math.random() * what.length)];
     let randomWhen = when[Math.floor(Math.random() * when.length)];
 
-    let excusa = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
-    document.getElementById("excuse").innerHTML= excusa
+    let excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+    document.getElementById("excuse").innerHTML = excuse;
   }
-  
-  ExcuseGenerator();
-  
+
+  generateExcuse();
+
+  document.getElementById("generateExcuse").addEventListener("click", generateExcuse);
+
+  document.getElementById("darkModeToggle").addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+
+    let icon = document.querySelector("#darkModeToggle i");
+    if (document.body.classList.contains("dark-mode")) {
+      icon.classList.remove("fa-moon");
+      icon.classList.add("fa-sun");
+    } else {
+      icon.classList.remove("fa-sun");
+      icon.classList.add("fa-moon");
+    }
+  });
 };
